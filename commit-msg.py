09a232cc217e9c9260d5e42224bdb918d67fb815
sakup <pukasbohdan@gmail.com>
2022-08-04 11:43:20 +0200

    initial commit

--- commit-msg.py
+++ commit-msg.py
@@ -2,11 +2,11 @@
 
 import sys
 
-def main()
+def main():
     print("Number of arguments: %d", len (sys.argv))
     print("Argument List: %s" % str (sys.argv))
 
-    sys.exit(1)
+    sys.exit(0)
 
 if __name__ == "__main__":
-    main() 
+    main()
