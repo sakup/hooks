3d821121809ee67f5f622854a454405a981c31be
sakup <pukasbohdan@gmail.com>
2022-08-04 11:35:12 +0200

    test

---
+++ commit-msg.py
@@ -0,0 +1,10 @@
+import sys
+
+def main()
+    print("Number of arguments: %d", len (sys.argv))
+    print("Argument List: %s" % str (sys.argv))
+
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
