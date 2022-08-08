437de1752d35cbaed82ca191dd383260da349a41
sakup <pukasbohdan@gmail.com>
2022-08-08 17:09:53 +0200

    fresh start

--- pre-receive.py
+++ pre-receive.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import fileinput
 
-# Read in each ref that the user is trying to update
-for line in fileinput.input():
-    print ("pre-receive: Trying to push ref: %s" % line)
-    print("Some text")
-
-# Abort the push
-# sys.exit(1)
+line = sys.stdin.read()
+print(line)
